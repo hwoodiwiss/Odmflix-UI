749e58a57e915b5e929a45f43f26a7f3dc28b9df
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-04-07 18:15:47 +0100

    Added local environment

--- src/environment.ts
+++ src/environment.ts
@@ -1,13 +1,20 @@
 export function getConfig(): Config {
+  let config: Config;
   if (process.env.NODE_ENV === "production") {
-    return {
+    config = {
       ApiUri: "https://api.odm.ws311470.remote.ac",
     };
-  } else {
-    return {
+  } else if (process.env.NODE_ENV === "development") {
+    config = {
       ApiUri: "https://dev.api.odm.ws311470.remote.ac",
     };
+  } else {
+    config = {
+      ApiUri: "http://localhost:1234",
+    };
   }
+
+  return config;
 }
 
 export interface Config {
