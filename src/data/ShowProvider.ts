ee2bfd6cea1a07f80c614ad2b0686a966196f283
Hugo Woodiwiss <2156707+hwoodiwiss@users.noreply.github.com>
2021-04-08 14:55:19 +0100

    Updated showprovider to use abstract base class

--- src/data/ShowProvider.ts
+++ src/data/ShowProvider.ts
@@ -1,23 +1,22 @@
 import "../import-jquery";
 import { Config } from "../environment";
 import { Show } from "./Show";
+import { Provider } from "./Provider";
 
-export class ShowProvider {
+export class ShowProvider extends Provider {
   private ApiUri: string;
   constructor(config: Config) {
+    super();
     this.ApiUri = config.ApiUri;
   }
 
   public ById(id: number) {
-    return fetch(`${this.ApiUri}/Show/ById?id=${id}`)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error(
-          `Error getting data from the API. Response code: ${response.status}`
-        );
-      })
-      .then<Show>((data) => data);
+    return this.fetchJson(`${this.ApiUri}/Show/ById?id=${id}`, "GET").then<Show>((data) => data);
+  }
+
+  public ByTypeName(typeName: string) {
+    return this.fetchJson(`${this.ApiUri}/Show/ByTypeName?typeName=${typeName}`, "GET").then<
+      Show[]
+    >((data) => data);
   }
 }
