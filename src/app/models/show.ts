d7a598d4f3d3e666327071728d9899e0ff3b4ad5
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-04-20 12:27:34 +0100

    Update show model

--- src/app/models/show.ts
+++ src/app/models/show.ts
@@ -1,31 +1,7 @@
 export interface Show {
   Id: number;
-  ShowTypeId: number;
-  ShowType: string;
-  Title: string;
-  DateAdded: string;
-  ReleaseYearId: number;
-  ReleaseYear: number;
-  RatingId: number;
-  Rating: string;
-  Duration: number;
-  Description: string;
-}
-
-export interface TvShow {
-  Id: number;
-  Title: string;
-  DateAdded: string;
-  ReleaseYearId: number;
-  ReleaseYear: number;
-  RatingId: number;
-  Rating: string;
-  Duration: number;
-  Description: string;
-}
-
-export interface Movie {
-  Id: number;
+  ShowTypeId?: number;
+  ShowType?: string;
   Title: string;
   DateAdded: string;
   ReleaseYearId: number;
