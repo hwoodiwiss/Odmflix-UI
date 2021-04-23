bb244ee1a47e5a3ca294dfb2aff5b104a5f801c5
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-04-23 22:27:37 +0100

    Started implementing bar chart component

--- src/app/charts/base/bar-chart/bar-chart.component.ts
+++ src/app/charts/base/bar-chart/bar-chart.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
+import { Chart } from "chart.js";
+import { ColourToRgb, ColourToRgba, datasetColours } from "../colours";
 
 @Component({
-  selector: 'ofui-bar-chart',
-  templateUrl: './bar-chart.component.html',
-  styleUrls: ['./bar-chart.component.scss']
+  selector: "ofui-bar-chart",
+  templateUrl: "./bar-chart.component.html",
+  styleUrls: ["./bar-chart.component.scss"],
 })
 export class BarChartComponent implements OnInit {
+  @ViewChild("chartCanvas") chartElement: ElementRef<HTMLCanvasElement>;
+  @Input() horizontal: boolean = false;
+  @Input() dataGenerator: () => { label: string; data: number[] }[];
+  chart: Chart;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.chart = new Chart(this.chartElement.nativeElement, {
+      type: "bar",
+      data: {
+        datasets: this.getDataSets(),
+      },
+      options: {
+        scales: this.getScalesConfig(),
+        indexAxis: this.getIndexAxis(),
+      },
+    });
   }
 
+  getScalesConfig() {
+    let scalesObj;
+    if (this.horizontal) {
+      scalesObj = {
+        y: {
+          beginAtZero: true,
+        },
+      };
+    } else {
+      scalesObj = {
+        x: {
+          beginAtZero: true,
+        },
+      };
+    }
+
+    return scalesObj;
+  }
+
+  getIndexAxis() {
+    return this.horizontal ? "y" : "x";
+  }
+
+  getDataSets() {
+    const datasets = this.dataGenerator();
+    return datasets.map((dataset, index) => {
+      const colour = datasetColours[index % datasetColours.length];
+      const chartDatasetConfig = {
+        backgroundColor: new Array(dataset.data.length).fill(
+          ColourToRgba(colour, 0.2)
+        ),
+        borderColor: new Array(dataset.data.length).fill(ColourToRgb(colour)),
+      };
+
+      return { ...chartDatasetConfig, ...dataset };
+    });
+  }
 }
---
+++ src/app/charts/base/colours.ts
@@ -0,0 +1,17 @@
+type Colour = [number, number, number];
+
+const green: Colour = [25, 169, 116];
+const purple: Colour = [94, 44, 165];
+const yellow: Colour = [255, 200, 50];
+const red: Colour = [255, 65, 54];
+const blue: Colour = [66, 153, 191];
+
+export function ColourToRgb(colour: Colour): string {
+  return `rgb(${colour[0]}, ${colour[1]}, ${colour[2]})`;
+}
+
+export function ColourToRgba(colour: Colour, opacity: number): string {
+  return `rgba(${colour[0]}, ${colour[1]}, ${colour[2]}, ${opacity})`;
+}
+
+export const datasetColours = [green, purple, yellow, red, blue];
