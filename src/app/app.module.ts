eac7ba556255c69e5633107973a3d6d52cc47753
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-04-24 01:46:40 +0100

    Add chart registration to AppModule to avoid dead code removal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,26 @@ import { AppComponent } from "./app.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { NotFoundPageComponent } from "./not-found-page/not-found-page.component";
-import { TestChartComponent } from "./charts/test-chart/test-chart.component";
 import { MapChartComponent } from "./charts/base/map-chart/map-chart.component";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { MapYearsComponent } from "./charts/map-years/map-years.component";
-import { CountriesPageComponent } from './countries-page/countries-page.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { YearsPageComponent } from './years-page/years-page.component';
-import { PieChartComponent } from './charts/base/pie-chart/pie-chart.component';
-import { BarChartComponent } from './charts/base/bar-chart/bar-chart.component';
-import { PolarRadiusChartComponent } from './charts/base/polar-radius-chart/polar-radius-chart.component';
-import { YearCountryDataComponent } from './years-page/year-country-data/year-country-data.component';
+import { CountriesPageComponent } from "./countries-page/countries-page.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { YearsPageComponent } from "./years-page/years-page.component";
+import { PieChartComponent } from "./charts/base/pie-chart/pie-chart.component";
+import { BarChartComponent } from "./charts/base/bar-chart/bar-chart.component";
+import { PolarRadiusChartComponent } from "./charts/base/polar-radius-chart/polar-radius-chart.component";
+import { YearCountryDataComponent } from "./years-page/year-country-data/year-country-data.component";
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     NotFoundPageComponent,
-    TestChartComponent,
     MapChartComponent,
     MapYearsComponent,
     CountriesPageComponent,
